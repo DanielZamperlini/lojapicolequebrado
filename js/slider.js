598d4c889963cc9a07019ff28dc55243bbf99c2e
Daniel Santos <monkeydluffysan2021@gmail.com>
2024-12-10 22:56:02 -0300

    test

--- js/slider.js
+++ js/slider.js
@@ -3,15 +3,15 @@ export class Slider {
     this.currentSlide = 0;
     this.slides = [
       {
-        image: '/imgs/gui-noseslide.jpg',
+        image: 'imgs/gui-noseslide.jpg',
         caption: 'Wesley "Gnomo"',
       },
       {
-        image: '/imgs/camuflado.png',
+        image: './imgs/camuflado.png',
         caption: 'Shapes Maple',
       },
       {
-        image: '/imgs/latino-rock.jpg',
+        image: '../imgs/latino-rock.jpg',
         caption: 'Daniel "Latino"',
       },
       {
